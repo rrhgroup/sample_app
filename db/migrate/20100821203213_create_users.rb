e67fcbcaacf25359ee26fb2ecb150feb36754c93
rrhg <rrhg.group@outlook.com>
2014-08-16 00:32:14 +0000

    Modified database script

--- db/migrate/20100821203213_create_users.rb
+++ db/migrate/20100821203213_create_users.rb
@@ -1,4 +1,5 @@
 class CreateUsers < ActiveRecord::Migration
+
   def self.up
     create_table :users do |t|
       t.string :name
@@ -6,9 +7,11 @@ class CreateUsers < ActiveRecord::Migration
 
       t.timestamps
     end
+
   end
 
   def self.down
     drop_table :users
   end
+
 end
